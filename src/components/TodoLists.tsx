f4bf6f758273d47fd043a43d6cfbbaa8f4182b1e
Vadim Barkovsky <v.barkovsky@itransition.com>
2021-05-15 15:25:05 +0300

    Fixed bugs

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -6,56 +6,67 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      overflow: 'hidden',
-      padding: theme.spacing(0, 3)
-      },
-    paper: {
-      maxWidth: '100%',
-      margin: `${theme.spacing(1)}px auto`,
-      padding: theme.spacing(2),
-    },
-    button: {
-      margin: theme.spacing(1),
-    },
-    })
-  );
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
+  },
+  paper: {
+    maxWidth: '100%',
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 interface TodoListProps {
     todo: {id: string, text: string}[];
     onDeleteTodo: (id: string) => void;
-  };
+  }
 
-const TodoLists: React.FC<TodoListProps> = ({todo, onDeleteTodo}) => {
-    const classes = useStyles();
+const TodoLists: React.FC<TodoListProps> = ({ todo, onDeleteTodo }) => {
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            {todo && todo.map(todo => {
-              return(
-                <Paper key={todo.id} className={classes.paper}>
-                  <Grid container direction="column" wrap="nowrap" spacing={2}>
-                    <Grid item xs>
-                      <Typography variant="caption" display="block" gutterBottom>
-                        {todo.id}
-                      </Typography>
-                      <Typography noWrap>
-                        {todo.text}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs>
-                      <Button className={classes.button} variant="contained" color="primary" size="small">Edit</Button>
-                      <Button onClick={onDeleteTodo.bind(null, todo.id)} className={classes.button} variant="contained" color="secondary" size="small">Delete</Button>
-                    </Grid>
-                  </Grid>
-                </Paper>
-              )}
-            )}
-        </div>
-    )
-}
+  return (
+    <div className={classes.root}>
+      {todo && todo.map((todo) => (
+        <Paper key={todo.id} className={classes.paper}>
+          <Grid container direction="column" wrap="nowrap" spacing={2}>
+            <Grid item xs>
+              <Typography variant="caption" display="block" gutterBottom>
+                {todo.id}
+              </Typography>
+              <Typography noWrap>
+                {todo.text}
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <Button
+                className={classes.button}
+                variant="contained"
+                color="primary"
+                size="small"
+              >
+                Edit
+              </Button>
+              <Button
+                onClick={onDeleteTodo.bind(null, todo.id)}
+                className={classes.button}
+                variant="contained"
+                color="secondary"
+                size="small"
+              >
+                Delete
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      ))}
+    </div>
+  );
+};
 
 export default TodoLists;
--- src/conteiners/App.tsx
+++ src/conteiners/App.tsx
@@ -1,61 +1,46 @@
 import * as React from 'react';
 
+import Container from '@material-ui/core/Container';
+import Alert from '@material-ui/lab/Alert';
 import { Todo } from './todo.models';
 
 import TodoList from '../components/TodoLists';
 import NewTodo from '../components/NewTodo';
 
-import Container from '@material-ui/core/Container';
-import Snackbar from "@material-ui/core/Snackbar";
-import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
-
-function Alert(props: AlertProps) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
 const App:React.FC = () => {
-    const [todos, setTodos] = React.useState<Todo[]>([
-      {id: '5/15/2021, 11:38:04 AM', text: 'Hello'},
-      {id: '6/12/2020, 11:12:04 PM', text: 'Hello 2'},
-      {id: '7/24/2020, 10:40:00 PM', text: 'Hello 3'},
-    ]);
+  const [todos, setTodos] = React.useState<Todo[]>([
+    { id: '5/15/2021, 11:38:04 AM', text: 'Hello' },
+    { id: '6/12/2020, 11:12:04 PM', text: 'Hello 2' },
+    { id: '7/24/2020, 10:40:00 PM', text: 'Hello 3' },
+  ]);
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpenModal = () => {
-    setOpen(true);
-  };
-  const handleCloseModal = (event?: React.SyntheticEvent, reason?: string) => {
-    if (reason === "clickaway") {
+  const [alarm, setAlarm] = React.useState(false);
+
+  const todoAddHandler = (text: string) => {
+    if (text === '') {
+      setAlarm(true);
+      setTimeout(() => setAlarm(false), 1000);
       return;
     }
-    setOpen(false);
+    const newTodo = {
+      id: new Date().toLocaleString(),
+      text,
+    };
+    setTodos((prefTodos) => [...prefTodos, newTodo]);
   };
 
-    const todoAddHandler = (text: string) => {
-      if (text === '') {
-        handleOpenModal();
-        return;
-      }
-      setTodos(prefTodos => [...prefTodos, {id: new Date().toLocaleString(), text: text}]);
-    };
+  const todoDeletehandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((item) => item.id !== todoId));
+  };
 
-    const todoDeletehandler = (todoId: string) => {
-      setTodos(prevTodos => {
-        return prevTodos.filter(item => item.id !== todoId)
-      })
-    }
+  return (
+    <Container className="App" maxWidth="xl">
+      { alarm && <Alert variant="filled" severity="error">Add text!</Alert>}
 
-    return (
-          <Container className="App" maxWidth="xl">
-
-            <Snackbar open={open} autoHideDuration={3000} onClose={handleCloseModal}>
-              <Alert onClose={handleCloseModal} severity="error">Add text!</Alert>
-            </Snackbar>
-
-            <NewTodo onAddTodo={todoAddHandler}/>
-            <TodoList todo={todos} onDeleteTodo={todoDeletehandler}/>
-          </Container>
-    )
-}
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList todo={todos} onDeleteTodo={todoDeletehandler} />
+    </Container>
+  );
+};
 
 export default App;
