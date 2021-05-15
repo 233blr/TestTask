ce425ece66cef06a2c216731d168d344d3596a9e
Vadim Barkovsky <v.barkovsky@itransition.com>
2021-05-15 16:00:27 +0300

    Added the ability to save to LocalStorage

--- src/conteiners/App.tsx
+++ src/conteiners/App.tsx
@@ -2,24 +2,44 @@ import * as React from 'react';
 
 import Container from '@material-ui/core/Container';
 import Alert from '@material-ui/lab/Alert';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 import { Todo } from './todo.models';
 
 import TodoList from '../components/TodoLists';
 import NewTodo from '../components/NewTodo';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  alarm: {
+    position: 'fixed',
+    bottom: 20
+  }
+}));
+
 const App:React.FC = () => {
-  const [todos, setTodos] = React.useState<Todo[]>([
-    { id: '5/15/2021, 11:38:04 AM', text: 'Hello' },
-    { id: '6/12/2020, 11:12:04 PM', text: 'Hello 2' },
-    { id: '7/24/2020, 10:40:00 PM', text: 'Hello 3' },
-  ]);
+  const classes = useStyles();
+  
+  const [todos, setTodos] = React.useState<Todo[]>([]);
 
   const [alarm, setAlarm] = React.useState(false);
+
+  React.useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const json = JSON.stringify(todos);
+    localStorage.setItem("todos", json);
+  }, [todos]);
 
   const todoAddHandler = (text: string) => {
     if (text === '') {
       setAlarm(true);
-      setTimeout(() => setAlarm(false), 1000);
+      setTimeout(() => setAlarm(false), 3000);
       return;
     }
     const newTodo = {
@@ -35,7 +55,7 @@ const App:React.FC = () => {
 
   return (
     <Container className="App" maxWidth="xl">
-      { alarm && <Alert variant="filled" severity="error">Add text!</Alert>}
+      { alarm && <Alert className={classes.alarm} variant="filled" severity="error">No text!</Alert>}
 
       <NewTodo onAddTodo={todoAddHandler} />
       <TodoList todo={todos} onDeleteTodo={todoDeletehandler} />
